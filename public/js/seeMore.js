6755404885332b42cc27fc8696b2864414ebb085
crepechoco57 <sarah.herberichs@neuf.fr>
2024-03-19 16:33:07 +0100

    190324

--- public/js/seeMore.js
+++ public/js/seeMore.js
@@ -1,34 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var shortText = document.querySelectorAll(".short-text");
-//   var fullText = document.querySelectorAll(".full-text");
-//   var seeMoreTextBtn = document.querySelectorAll(".btn-more-text");
-//   var reduceTextBtn = document.querySelectorAll(".btn-reduce-text");
 
-//   console.log(shortText.length);
-//   if (shortText.textContent < 250) {
-//     fullText.style.display = "none";
-//   }
-
-//   reduceTextBtn.style.display = "none";
-//   fullText.style.display = "none";
-
-//   seeMoreTextBtn.addEventListener("click", function () {
-//     shortText.style.display = "none";
-//     fullText.style.display = "block";
-//     reduceTextBtn.style.display = "inline-block";
-//   });
-
-//   reduceTextBtn.addEventListener("click", function () {
-//     shortText.style.display = "block";
-//     fullText.style.display = "none";
-//     reduceTextBtn.style.display = "none";
-//   });
-
-//   // Vérifie initialement si le texte est tronqué pour afficher ou non le bouton "Réduire"
-//   if (shortText.innerText.length < message.text.length) {
-//     reduceTextBtn.style.display = "inline-block";
-//   }
-// });
 document.addEventListener("DOMContentLoaded", function () {
   var shortTexts = document.querySelectorAll(".short-text");
   var fullTexts = document.querySelectorAll(".full-text");
